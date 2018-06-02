15779326a1f8a25574681bb27a017fa70ea6c314
y1114039812 <1114039812@qq.com>
2018-06-02 18:43:29 +0800

    test

--- web.sh
+++ web.sh
@@ -2,12 +2,12 @@
 for Jmx in `ls web/jmx`
 do
 	num=`echo $Jmx|awk -F'_' '{print $1}'`
-	Jtl="./web/jtl/${num}_jdbc.jtl"
+	Jtl="./web/jtl/${num}_root.jtl"
 #	echo -e "$Jmx $num $Jtl\n"
 	if [ -f $Jtl ];then
 		continue
 	else
-		/usr/local/jmeter-4.0/bin/jmeter -n -t ./web/jmx/${num}_jdbc.jmx -l ./web/jtl/${num}_jdbc.jtl
+		/usr/local/jmeter-4.0/bin/jmeter -n -t ./web/jmx/${num}_root.jmx -l ./web/jtl/${num}_root.jtl
 	fi
 done
 git add --all
