19fac5a81e9ba6c5d924b1814740b36f52976449
Administrator <admin@local.host>
2018-06-02 19:00:42 +0800

    test

--- mysql.sh
+++ mysql.sh
@@ -2,12 +2,13 @@
 for Jmx in `ls mysql/jmx`
 do
 	num=`echo $Jmx|awk -F'_' '{print $1}'`
-	Jtl="./mysql/jtl/${num}_jdbc.jtl"
+	name=`echo $Jmx|awk -F'[_.]' '{print $2}'`
+	Jtl="./mysql/jtl/${num}_${name}.jtl"
 #	echo -e "$Jmx $num $Jtl\n"
 	if [ -f $Jtl ];then
 		continue
 	else
-		/usr/local/jmeter-4.0/bin/jmeter -n -t ./mysql/jmx/${num}_jdbc.jmx -l ./mysql/jtl/${num}_jdbc.jtl
+		/usr/local/jmeter-4.0/bin/jmeter -n -t ./mysql/jmx/${num}_${name}.jmx -l ./mysql/jtl/${num}_${name}.jtl
 	fi
 done
 git add --all
