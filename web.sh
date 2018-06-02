77c6b96bb60c8f81c75664d518aec52781b218c2
Administrator <admin@local.host>
2018-06-02 19:01:55 +0800

    a

--- web.sh
+++ web.sh
@@ -2,12 +2,13 @@
 for Jmx in `ls web/jmx`
 do
 	num=`echo $Jmx|awk -F'_' '{print $1}'`
-	Jtl="./web/jtl/${num}_root.jtl"
+	name=`echo $Jmx|awk -F'[_.]' '{print $2}'`
+	Jtl="./web/jtl/${num}_${name}.jtl"
 #	echo -e "$Jmx $num $Jtl\n"
 	if [ -f $Jtl ];then
 		continue
 	else
-		/usr/local/jmeter-4.0/bin/jmeter -n -t ./web/jmx/${num}_root.jmx -l ./web/jtl/${num}_root.jtl
+		/usr/local/jmeter-4.0/bin/jmeter -n -t ./web/jmx/${num}_${name}.jmx -l ./web/jtl/${num}_${name}.jtl
 	fi
 done
 git add --all
